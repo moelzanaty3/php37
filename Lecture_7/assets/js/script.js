0655e532f48bb9ce8edf319a9c0a17b58f9be6e6
MohammedElzanaty <mohammedelzanaty129@gmail.com>
2019-12-16 21:15:23 +0200

    if condition

--- Lecture_7/assets/js/script.js
+++ Lecture_7/assets/js/script.js
@@ -1,18 +1,52 @@
-const numberOne = 12;
-const numberTwo = 2; 
+/*
+  write a program to calculate your grade / GPA
+  1. Declare a variable for grade 
+  2. check 
+     if grade < 0 || grade > 100
+            if grade < 0   => negative 
+            else grade > 100 => greater than 100
+     else 
+        if      grade < 50                   => fail
+        else if grade >= 50 && grad < 65    => good 
+        else if grade >= 65 && grad < 85    => very good 
+        else    grade >= 85                  => Excellent  
+*/
+// if(condition) {}
+// else if(condition) {}
+// else {}
 
 
-const sum = numberOne + numberTwo; // 24 
-const sub = numberOne - numberTwo; // 16 
-const mul = numberOne * numberTwo; // 80 
-const div = numberOne / numberTwo; // 5 
-const rem = numberOne % numberTwo; // 0 
-const power = numberOne ** numberTwo; // .. 
 
 
-console.log(sum);
-console.log(sub);
-console.log(div);
-console.log(mul);
-console.log(rem);
-console.log(power);
+
+
+
+
+
+
+
+
+
+
+
+const grade = 120;
+// grade less than 0 OR grade greater than 100
+if (grade < 0 || grade > 100) {
+  // negative
+  if (grade < 0) {
+    console.log('negative');
+  } else {
+    // number > 100
+    console.log('greater than 100');
+  }
+} else {
+  if (grade >= 0 && grade < 50) {
+    console.log('fail');
+  } else if (grade >= 50 && grade < 65) {
+    console.log('good');
+  } else if (grade >= 65 && grade < 85) {
+    console.log('very good');
+  } else if (grade >= 85 && grade <= 100) {
+    console.log('Excellent');
+  }
+}
