803dfe2a0f88b8f2e96f14cdabf01e5e50ef88ae
MohammedElzanaty <mohammedelzanaty129@gmail.com>
2019-12-16 19:26:33 +0200

    make a simple calculator

---
+++ Lecture_7/assets/js/script.js
@@ -0,0 +1,18 @@
+const numberOne = 12;
+const numberTwo = 2; 
+
+
+const sum = numberOne + numberTwo; // 24 
+const sub = numberOne - numberTwo; // 16 
+const mul = numberOne * numberTwo; // 80 
+const div = numberOne / numberTwo; // 5 
+const rem = numberOne % numberTwo; // 0 
+const power = numberOne ** numberTwo; // .. 
+
+
+console.log(sum);
+console.log(sub);
+console.log(div);
+console.log(mul);
+console.log(rem);
+console.log(power);
