00eae068178ebc52f97a4ecc273803421b81665c
MohammedElzanaty <mohammedelzanaty129@gmail.com>
2019-12-18 19:00:45 +0200

    stackoverflow files

---
+++ file.js
@@ -0,0 +1,535 @@
+const projects = [
+  {
+    projectId: 1635,
+    projectOldId: 'P189211',
+    projectDescription: '356 Speedster',
+    projectBusinessPartnerName: 'Rafael Diez',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 1668,
+    projectOldId: 'P189056',
+    projectDescription: 'Vergabe Modellbepastung (Ticketsystem)',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 1938,
+    projectOldId: 'P189293',
+    projectDescription: 'Digitale Stylingmodelle',
+    projectBusinessPartnerName: 'Dr. Ing. h.c. F. Porsche AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2353,
+    projectOldId: 'P188989',
+    projectDescription: 'Entwicklung HENK CRUISER',
+    projectBusinessPartnerName: 'Henk B.V.',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2534,
+    projectOldId: 'P199956',
+    projectDescription: 'Allgemine Strakleistung 2019\r\n(O-0000061712 - 1)',
+    projectBusinessPartnerName: 'AKKA GmbH & Co.KGaA',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2537,
+    projectOldId: 'P199959',
+    projectDescription: 'Jetson Gehäuse - E/E Bestückung',
+    projectBusinessPartnerName: 'Robert Bosch GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2538,
+    projectOldId: 'P199960',
+    projectDescription: 'Aluminiumfelge Spyder',
+    projectBusinessPartnerName: 'Dr. Ing. h.c. F. Porsche AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2540,
+    projectOldId: 'P199962',
+    projectDescription: 'Lenkrad ZF, Reparatur',
+    projectBusinessPartnerName: 'studiokurbos GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2541,
+    projectOldId: 'P199963',
+    projectDescription: 'Aluminium-Frästeile',
+    projectBusinessPartnerName: 'creative Wave GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2542,
+    projectOldId: 'P199964',
+    projectDescription: 'V295 - Exterieur Anbauteile',
+    projectBusinessPartnerName: 'Mercedes-Benz Customer Solutions GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2559,
+    projectOldId: 'P199938',
+    projectDescription: 'BMW G87 FeasCub\r\n(O-0000059382 - 1)',
+    projectBusinessPartnerName: 'BMW M GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2561,
+    projectOldId: 'P199940',
+    projectDescription: 'C192 EM\r\n(O-0000059549 - 1)',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2573,
+    projectOldId: 'P199973',
+    projectDescription: 'VS20 - Mopf 2 Modellvorbereitung V-Klasse',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2576,
+    projectOldId: 'P199998',
+    projectDescription: 'E3 PA Ext. FKM Basismodell',
+    projectBusinessPartnerName: 'Dr. Ing. h.c. F. Porsche AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2577,
+    projectOldId: 'P199999',
+    projectDescription: 'Ineos Vorbereitungen Review 12.11.2019',
+    projectBusinessPartnerName: 'INEOS Automotive Limited',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2584,
+    projectOldId: 'P199997',
+    projectDescription: 'PO 633 Ext. FKM Basismodell',
+    projectBusinessPartnerName: 'Dr. Ing. h.c. F. Porsche AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2596,
+    projectOldId: 'P199996',
+    projectDescription: 'G3 Panamera Stylingmodell Überarbeitung',
+    projectBusinessPartnerName: 'Dr. Ing. h.c. F. Porsche AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2597,
+    projectOldId: 'P199994',
+    projectDescription: 'Heckleuchte VE - Varianten Deckglas',
+    projectBusinessPartnerName: 'Dr. Ing. h.c. F. Porsche AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2600,
+    projectOldId: 'P199978',
+    projectDescription: 'FCE IP CES2020',
+    projectBusinessPartnerName: 'Faurecia Innenraum Systeme GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2602,
+    projectOldId: 'P199980',
+    projectDescription: 'E-Mirror CES2020',
+    projectBusinessPartnerName: 'Faurecia Innenraum Systeme GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2603,
+    projectOldId: 'P199981',
+    projectDescription: 'Oldtimerfotografie - Mercedes Classic - D. Heck\r\n(O-0000064501 - 1)',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2604,
+    projectOldId: 'P199982',
+    projectDescription: 'SC033,  Nachbau Dachaubau\r\n(O-0000059560 - 1)',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2607,
+    projectOldId: 'P199984',
+    projectDescription: 'GT3 RS Alufelgen',
+    projectBusinessPartnerName: 'Dr. Ing. h.c. F. Porsche AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2610,
+    projectOldId: 'P199988',
+    projectDescription: 'V297 DKM 2 Exterieur Black Panel Lidar',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2612,
+    projectOldId: 'P199990',
+    projectDescription: 'Filmdreh/ Fotoshooting SC040\r\nCilia Tournier\r\n(O-0000061503 - 1)',
+    projectBusinessPartnerName: 'fischerAppelt, play GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2614,
+    projectOldId: 'P199992',
+    projectDescription: 'Alphard Fighter SC in Clay auftragen',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2628,
+    projectOldId: 'P199866',
+    projectDescription: 'Aussenhaut Modulares Mock up',
+    projectBusinessPartnerName: 'Deutsches Zentrum für Luft-und Raumfahrt (DLR)-Stuttgart',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2642,
+    projectOldId: 'P199845',
+    projectDescription: 'Centerpiece 2020',
+    projectBusinessPartnerName: 'AKKA GmbH & Co.KGaA',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2643,
+    projectOldId: 'P199846',
+    projectDescription: 'SC040, Interieur Modell',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2650,
+    projectOldId: 'P199864',
+    projectDescription: 'Erweiterung NGC Urban Modular Vehicle (UMV) Fahrzeug',
+    projectBusinessPartnerName: 'Deutsches Zentrum für Luft-und Raumfahrt (DLR)-Stuttgart',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2659,
+    projectOldId: 'P199862',
+    projectDescription: 'X296 CCM Innomodell DBE',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2664,
+    projectOldId: 'P199911',
+    projectDescription: 'GLA Foto\r\n(O-0000059640 - 1)',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2677,
+    projectOldId: 'P199924',
+    projectDescription: 'E-Klasse Fotoshooting\r\nGerd Esser\r\n(O-0000059641 - 1)',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2689,
+    projectOldId: 'P199892',
+    projectDescription: 'U11 - FeasCub Front und Heck MSP\r\nWWE-1902070032',
+    projectBusinessPartnerName: 'BMW M GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2704,
+    projectOldId: 'P199898',
+    projectDescription: 'Ineos Grenadier LBW Double Pick up 1:1',
+    projectBusinessPartnerName: 'AKKA GmbH & Co.KGaA',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2880,
+    projectOldId: 'P199842',
+    projectDescription: 'Kaege Carbon blau',
+    projectBusinessPartnerName: 'Kaege Automobile GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2881,
+    projectOldId: 'P199843',
+    projectDescription: 'Kaege Carbon silber',
+    projectBusinessPartnerName: 'Kaege Automobile GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2912,
+    projectOldId: 'P199588',
+    projectDescription: 'CFK Humidor, Aufarbeitung',
+    projectBusinessPartnerName: 'Henk B.V.',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2917,
+    projectOldId: 'P199593',
+    projectDescription: 'Bepastung Aerodynamikmodellen',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2937,
+    projectOldId: 'P199657',
+    projectDescription: 'V-Klasse Anbauteile und Stoßfänger hinten',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2959,
+    projectOldId: 'P199635',
+    projectDescription: 'SC039',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2968,
+    projectOldId: 'P199841',
+    projectDescription: '911 Turbo rot',
+    projectBusinessPartnerName: 'Rafael Diez',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2978,
+    projectOldId: 'P1910001',
+    projectDescription: 'CID Glas ',
+    projectBusinessPartnerName: 'GERMANEERS GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2981,
+    projectOldId: 'P1910004',
+    projectDescription: 'FeasCub SBBR',
+    projectBusinessPartnerName: 'Ingolstädter Fräszentrum GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2982,
+    projectOldId: 'P1910005',
+    projectDescription: 'Stützrahmen \r\n(O-0000064092 - 1)',
+    projectBusinessPartnerName: 'Kunststofftechnik Backhaus GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2984,
+    projectOldId: 'P1910007',
+    projectDescription: 'X167, EM Display',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2986,
+    projectOldId: 'P1910009',
+    projectDescription: 'SC SBBR PMMA',
+    projectBusinessPartnerName: 'Ingolstädter Fräszentrum GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2988,
+    projectOldId: 'P1910011',
+    projectDescription: 'Vorentwicklungsprojekt Poseidon',
+    projectBusinessPartnerName: 'Daimler Truck AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2990,
+    projectOldId: 'P1910012',
+    projectDescription: 'FNC 1:1-Modell Aufbau 2019 & 2020',
+    projectBusinessPartnerName: 'Mercedes-Benz AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 2997,
+    projectOldId: 'P189517',
+    projectDescription:
+      'Fahrzeug Urban Modular Vehicle (UMV) Peoplemover mit zusätzlicher Plattform und Sitzkiste',
+    projectBusinessPartnerName: 'Deutsches Zentrum für Luft-und Raumfahrt (DLR)-Stuttgart',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3004,
+    projectOldId: 'P189524',
+    projectDescription: '300 SL GULLWING BONHAMS',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3016,
+    projectOldId: 'P199560',
+    projectDescription: 'Kaege Sauger, Kotflügel, Seitenwand, Stoßfänger',
+    projectBusinessPartnerName: 'Kaege Automobile GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3034,
+    projectOldId: 'P1910014',
+    projectDescription: 'SC SBBR Lichtsteine',
+    projectBusinessPartnerName: 'Ingolstädter Fräszentrum GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3037,
+    projectOldId: 'P1910016',
+    projectDescription: 'Design Detailshots \r\nRenate Riek-Bauer\t',
+    projectBusinessPartnerName: 'Mercedes-Benz AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3038,
+    projectOldId: 'P1910017',
+    projectDescription: 'Plexiteil mit 7 Facetten polieren',
+    projectBusinessPartnerName: 'Reisinger Modellbau GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3039,
+    projectOldId: 'P1910018',
+    projectDescription: 'G-Wagon Virgil Abloh',
+    projectBusinessPartnerName: 'Mercedes-Benz AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3044,
+    projectOldId: 'P199544',
+    projectDescription: 'Farbmessung - Bauteile, Grundmuster, Archiv\r\n2019',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3084,
+    projectOldId: 'P199761',
+    projectDescription: 'FeasCub G07 LCI AG\r\nWWE-1902120004',
+    projectBusinessPartnerName: 'BMW AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3114,
+    projectOldId: 'P199834',
+    projectDescription: 'FeasCub U11 - Front\r\nWWE-1902070032',
+    projectBusinessPartnerName: 'BMW AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3115,
+    projectOldId: 'P199835',
+    projectDescription: 'FeasCub U11 - Heck\r\nWWE-1902070032',
+    projectBusinessPartnerName: 'BMW AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3126,
+    projectOldId: 'P199802',
+    projectDescription: 'SC040 Radkonzept',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3137,
+    projectOldId: 'P199813',
+    projectDescription: 'ANÜ Concept modeling',
+    projectBusinessPartnerName: 'AKKA GmbH & Co.KGaA',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3142,
+    projectOldId: 'P199809',
+    projectDescription: 'Volksbus 1:10 Modell ',
+    projectBusinessPartnerName: 'Volkswagen AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3155,
+    projectOldId: 'P199699',
+    projectDescription: 'Allgemeine Bearbeitung',
+    projectBusinessPartnerName: 'Sousa CarDesign GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3184,
+    projectOldId: 'P199685',
+    projectDescription: 'SC040 CES 2020',
+    projectBusinessPartnerName: 'Daimler AG',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  },
+  {
+    projectId: 3223,
+    projectOldId: 'P199724',
+    projectDescription: '16-125-19 Vision2025-Sandwichwelle',
+    projectBusinessPartnerName: 'GreMod GmbH',
+    isFavorite: false,
+    entryLastUpdateTime: null
+  }
+];
+
+projects.sort((a,b) => (a.projectOldId > b.projectOldId) ? 1 : ((b.projectOldId > a.projectOldId) ? -1 : 0)); 
+
+console.log(projects)
